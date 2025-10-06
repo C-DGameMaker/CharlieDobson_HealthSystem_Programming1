40cf203ff10fbbcb56c4df835a58e5b93fc4e7ca
C-DGameMaker <charlotte5634@outlook.com>
2025-10-06 10:19:41 -0300

    Initial commit

---
+++ CharlieDobson_HealthSystem_Programming1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharlieDobson_HealthSystem_Programming1
+{
+    internal class Program
+    {
+        //player stats
+        static int health = 100;
+        static string healthStatus = "";
+        static int shield = 100;
+        static int lives = 3;
+
+        static int xp = 0;
+        static int level = 1;
+        static void Main(string[] args)
+        {
+           
+
+        }
+
+        
+    }
+}
+
